a66e20ab8507e8b2ca45c688ac3b896bd6eb3b90
sonhoang205 <hson20052001@gmail.com>
2024-06-09 10:38:34 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import { CsvData } from "./utilities/types";
 import { csvToObjects } from "./utilities/utilis";
 import { Box } from "@mui/material";
-// import Chart from "react-apexcharts";
 import ReactApexChart from "react-apexcharts";
 import { format } from "date-fns";
 
 const App: React.FC = () => {
   const [data, setData] = useState<CsvData[]>([]);
-  console.log("data: ", data);
 
   useEffect(() => {
     fetch("/VFS_historical_data_StockScan.csv")
@@ -21,10 +19,9 @@ const App: React.FC = () => {
 
   return (
     <Box display={"flex"} flexDirection={"column"}>
-      <h1>CSV Data as Key-Value Objects</h1>
       <ReactApexChart
-        // type="candlestick"
-        series={{ data: data, name: "candle" }}
+        type="candlestick"
+        series={[{ name: "candles", data: data }]}
         options={{
           chart: {
             height: 350,
@@ -34,25 +31,6 @@ const App: React.FC = () => {
             text: "CandleStick Chart - Category X-axis",
             align: "left",
           },
-          annotations: {
-            xaxis: [
-              {
-                x: "Oct 06 14:00",
-                borderColor: "#00E396",
-                label: {
-                  borderColor: "#00E396",
-                  style: {
-                    fontSize: "12px",
-                    color: "#fff",
-                    background: "#00E396",
-                  },
-                  orientation: "horizontal",
-                  offsetY: 7,
-                  text: "Annotation Test",
-                },
-              },
-            ],
-          },
           tooltip: {
             enabled: true,
           },
@@ -60,9 +38,10 @@ const App: React.FC = () => {
             type: "category",
             labels: {
               formatter: function (val) {
-                return format(val, "MMM DD HH:mm");
+                return format(Number(val), "dd/MM/yyyy");
               },
             },
+            range: 6,
           },
           yaxis: {
             tooltip: {
